1af02a09fac46dff32a4a6ff9d211f7ae7f2375b
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-11-30 12:19:25 +0000

    res

---
+++ Models/EvolutionInnovation/openmole/runCalib.sh
@@ -0,0 +1,4 @@
+openmole -p ../target/scala-2.13/urbanevolution_2.13-0.1-SNAPSHOT.jar --password-file ~/.oml --script Calibration_innovHierarchy0-5.oms
+openmole -p ../target/scala-2.13/urbanevolution_2.13-0.1-SNAPSHOT.jar --password-file ~/.oml --script Calibration_innovHierarchy1-5.oms
+openmole -p ../target/scala-2.13/urbanevolution_2.13-0.1-SNAPSHOT.jar --password-file ~/.oml --script Calibration_innovHierarchy1.oms
+
